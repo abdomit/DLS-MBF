fe78f70fed507b0db134c20fb196e04327ce71ad
Benoit Roche <benoit.roche@esrf.fr>
2024-12-16 15:53:44 +0100

    Move config_folder to meta-esrf repo

--- sites/ESRF/config_folder/home_host/sync/etc/systemd/system/script-esrf/host/starter-pre.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-INTF_IP=$(ip addr show $TANGO_INTF | grep inet | cut -d' ' -f6 | cut -d'/' -f1)
-
-cat /opt/host/.mbf-env - << EOF > /opt/host/.starter-env
-export ORBendPoint=giop:tcp:$INTF_IP:
-EOF
